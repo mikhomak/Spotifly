c7062af7a7b7d5ba5e5935b1e2e52a37e4da68e2
Misha <homakhaker@gmail.com>
2020-02-24 16:49:47 +0100

    Created connection container with a navigation item component

---
+++ spotifly/src/components/navigations/navigationItems/navigationItems.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+interface NavigationItemsProps{
+    kek? : string
+}
+
+export const NavigationItems : React.FC<NavigationItemsProps> = (props) => {
+    return (<div>mda</div>);
+};
---
+++ spotifly/src/containers/connection/connection.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Component } from 'react'
+import { NavigationItems } from '../../components/navigations/navigationItems/navigationItems';
+
+class Connection extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <NavigationItems />
+            </div>
+        );
+    }
+}
+
+export default Connection;
