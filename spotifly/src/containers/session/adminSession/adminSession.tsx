86f8ba7fcc601a63847a48554f16f4490447f04f
Misha <homakhaker@gmail.com>
2020-02-25 15:37:37 +0100

    Added navigationItems and  footer for sessions

--- spotifly/src/containers/session/adminSession/adminSession.tsx
+++ spotifly/src/containers/session/adminSession/adminSession.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Component } from 'react';
+import { NavigationItems } from '../../../components/navigations/navigationItems/navigationItems';
+import { Footer } from '../../../components/footer/footer';
+
 
 export class AdminSession extends Component {
 
@@ -7,7 +10,8 @@ export class AdminSession extends Component {
     render = () => {
         return (
             <div>
-
+                <NavigationItems />
+                <Footer />
             </div>
         );
     };
--- spotifly/src/containers/session/clientSession/clientSession.tsx
+++ spotifly/src/containers/session/clientSession/clientSession.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
+import {NavigationItems} from '../../../components/navigations/navigationItems/navigationItems';
+import {Footer} from '../../../components/footer/footer';
 
 export class AdminSession extends Component {
 
@@ -7,7 +9,8 @@ export class AdminSession extends Component {
     render = () => {
         return (
             <div>
-
+                <NavigationItems />
+                <Footer />
             </div>
         );
     };
